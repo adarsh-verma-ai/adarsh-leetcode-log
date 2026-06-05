8b4b0c86d64656d7c84b98dba389a2a6a8c2ceeb
Adarsh Verma <vermaexperience@gmail.com>
2026-06-05 23:47:56 +0530

    Time: 0 ms (100%), Space: 42.2 MB (16.38%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,20 @@
+class Solution {
+    private int[] dp;
+
+    public int climbStairs(int n) {
+        dp = new int[n + 1];
+        return solve(n);
+    }
+
+    private int solve(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+
+        return dp[n] = solve(n - 1) + solve(n - 2);
+    }
+}
