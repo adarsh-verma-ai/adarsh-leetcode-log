43cc386cc440a68986be9120514cb7d979ccda63
Adarsh Verma <vermaexperience@gmail.com>
2026-06-29 20:49:30 +0530

    Time: 0 ms (100%), Space: 45.3 MB (10.72%) - LeetHub

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return buildTree(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildTree(int[] nums, int start, int end) {
+
+        // Base case
+        if (start > end) {
+            return null;
+        }
+
+        // Find the middle element
+        int mid = start + (end - start) / 2;
+
+        // Create root node
+        TreeNode root = new TreeNode(nums[mid]);
+
+        // Build left subtree
+        root.left = buildTree(nums, start, mid - 1);
+
+        // Build right subtree
+        root.right = buildTree(nums, mid + 1, end);
+
+        return root;
+    }
+}
